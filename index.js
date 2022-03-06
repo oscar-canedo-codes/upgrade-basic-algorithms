a41a933ebc23209ab92cb4152e006987a03fee05
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-03-06 15:07:45 +0100

    Iteration#2

--- index.js
+++ index.js
@@ -6,4 +6,29 @@ const y = 10;
 
 const z = h + y;
 
+// Iteration 2
+
+const firstName = "Jon";
+const lastName = "Snow";
+const age = 24;
+
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.`);
+
+//1.3
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+console.log(toy1.price + toy2.price);
+
+//1.4
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = 25000;
+
+// Update final price of car1 and car2
+car1.finalPrice = globalBasePrice + car1.basePrice;
+car2.finalPrice = globalBasePrice + car2.basePrice;
+
 
